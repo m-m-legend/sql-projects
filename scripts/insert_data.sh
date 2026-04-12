3164020099b213a499fff86d5a55f49ab7e14739
m-m-legend <nintendo_64m@outlook.com>
2026-04-12 19:28:24 -0300

    Correção de comentários dos projetos

--- scripts/insert_data.sh
+++ scripts/insert_data.sh
@@ -34,7 +34,7 @@ do
       OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     fi
 
-    # Inserir jogo
+    # Inserir game
     INSERT_GAME=$($PSQL "
       INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals)
       VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)
